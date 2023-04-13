871c5d1af37bbb11c8c66bf6b1afa3c048cd048d
Sam Walker <sgwalker327@gmail.com>
2023-04-13 17:05:58 -0600

    Fixed sloppy mistake with file transfer

---
+++ spec/features/users/new_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe '/users/new', type: :feature do
+  describe 'as a visitor' do
+    context 'when I visit the new user page' do
+      it 'I see a form to create a new user' do
+        visit new_user_path
+
+        expect(current_path).to eq(new_user_path)
+        expect(page).to have_content('Your Journey Start Here!')
+        expect(page).to have_field(:name)
+        expect(page).to have_field(:email)
+        expect(page).to have_select(:protocol_id)
+        expect(page).to have_select(:allow_sharing)
+        expect(page).to have_button("Begin My Journey")
+      end
+
+      it 'when I fill out the form and click submit, I am redirected to the user dashboard' do
+        visit new_user_path
+
+        fill_in :name, with: 'Bob'
+        fill_in :email, with: 'Bob@bob.com'
+        select 'Fadiman Protocol', from: :protocol_id
+        select 'Yes', from: :allow_sharing
+        click_button 'Begin My Journey'
+
+        expect(current_path).to eq(user_path(User.last))
+      end
+    end
+  end
+end
