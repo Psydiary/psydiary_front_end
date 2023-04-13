62694c88d4e78f136b719c0fcea16c2451106c96
Sam Walker <sgwalker327@gmail.com>
2023-04-13 17:00:51 -0600

    Added controller for users with new and create actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Please fill out all fields."
+      redirect_to new_user_path
+    end
+  end
+end
