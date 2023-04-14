5e104d7e2878a592ef8fae11a72efe810ed59e5d
David Marino <davidjmarino8@gmail.com>
2023-04-13 20:06:16 -0400

    fix: users edit route

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get '/users/:id/logs/new', to: "users/logs#new"
   post '/users/:id/logs', to: "users/logs#create"
+  get '/users/:id/edit', to: "users#edit"
+  patch '/users/:id', to: "users#update"
   get '/users/:id/logs', to: "users/logs#index"
   get '/users/:id/logs/:id', to: "users/logs#show"
   get '/users/:id/microdose_diarys', to: "users/microdose_diarys#index"
--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'User edit page' do
   it 'exists' do
-    visit '/protocols/edit'
+    visit '/users/1/edit'
     
-    expect(current_path).to eq('/protocols/edit')
+    expect(current_path).to eq('/users/1/edit')
     expect(page).to have_content('Edit Protocol')
   end
 end
