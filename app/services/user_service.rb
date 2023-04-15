ac6bf5b1eaa1cd67def7bf146e198211de48a31d
Sam Walker <sgwalker327@gmail.com>
2023-04-14 18:17:15 -0600

    Slight edit to UserService

--- app/services/user_service.rb
+++ app/services/user_service.rb
@@ -5,14 +5,10 @@ class UserService
     conn.post('/api/v1/users') do |req|
       req.body = user_params
     end
-    # post_url("/api/v1/users?#{user_params}")
+    
   end
 
   private
-
-  # def self.post_url(url)
-  #   response = conn.post(url)
-  # end
 
   def self.conn
     Faraday.new(url: 'http://localhost:3000/')
