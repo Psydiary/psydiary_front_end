994fdb844c2f2b74465636c0af10b0855b602080
Sam Walker <sgwalker327@gmail.com>
2023-04-15 13:41:54 -0600

    Rename service and adjust port for backend in connection method

---
+++ app/services/psydiary_service.rb
@@ -0,0 +1,15 @@
+require 'faraday'
+
+class PsiDiaryService
+  def self.create_user(user_params)
+    conn.post('/api/v1/users') do |req|
+      req.body = user_params
+    end
+  end
+
+  private
+
+  def self.conn
+    Faraday.new(url: 'http://localhost:4000/')
+  end
+end
