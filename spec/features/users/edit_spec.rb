3baf83480fb02cfbe35f216949a2b4abceb91c4e
Antonio King Hunt <89714398+4D-Coder@users.noreply.github.com>
2023-04-15 20:58:15 -0600

    Add: setup for user edit

--- spec/features/users/edit_spec.rb
+++ spec/features/users/edit_spec.rb
@@ -1,10 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'User edit page' do
-  it 'exists' do
-    visit '/users/1/edit'
-    
-    expect(current_path).to eq('/users/1/edit')
-    expect(page).to have_content('Edit Protocol')
+  describe 'As a visitor' do
+
   end
+
+  describe 'As a registered User' do
+    context 'When I visit the User Edit page (settings)' do
+      before do
+
+        visit '/users/1/settings'
+      end
+
+      it 'exists' do
+        expect(current_path).to eq('/users/1/settings')
+        save_and_open_page
+        within('id#profile_edit') do
+          expect(page).to have_field(:username)
+        end
+      end
+    end
+  end  
 end
