076513457b1ce688ab5afab877cf712d5a3d6936
Bobby Luly <bobbyluly@gmail.com>
2023-04-17 16:36:07 -0600

    Fix: vcr issues, comment vcr config line back in

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,7 +37,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.configure_rspec_metadata!
-  # config.allow_http_connections_when_no_cassette = true
+  config.allow_http_connections_when_no_cassette = true
 end
 
 RSpec.configure do |config|
