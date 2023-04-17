da95ac800a4bbf57fc12686ca7bb456aac95c96c
Sam Walker <sgwalker327@gmail.com>
2023-04-17 09:03:58 -0600

    Complete daily log entry service and test

--- app/facades/psydiary_facade.rb
+++ app/facades/psydiary_facade.rb
@@ -2,10 +2,11 @@ class PsydiaryFacade
 
   def initialize(params = {})
     @params = params
+    @service = PsydiaryService.new
   end
 
   def new_user
-   response = PsydiaryService.create_user(@params)
+   response = service.create_user(@params)
    user = JSON.parse(response.body, symbolize_names: true)
    return user[:errors] if user[:errors].present?
    User.new(user)
