a3ac1cd2bcc579978f8cb26cd2a42aac67f930b7
Sam Walker <sgwalker327@gmail.com>
2023-04-18 20:18:10 -0600

    add facade for entries in psydiary facade

--- app/facades/psydiary_facade.rb
+++ app/facades/psydiary_facade.rb
@@ -30,6 +30,22 @@ class PsydiaryFacade
     end
   end
 
+  def entries
+    response = PsydiaryService.get_entries(@params)
+    entries_data = JSON.parse(response.body, symbolize_names: true)
+    if !entries_data[:data].empty?
+      entries_data[:data].map do |entry|
+        if entry[:attributes][:mood]
+          DailyLogEntry.new(entry)
+        else
+          MicrodoseLogEntry.new(entry)
+        end
+      end
+    else
+      "Nothing here yet.... Make a new entry above!"
+    end
+  end
+
   def authenticate_user
     user = PsydiaryService.authenticate_user(@params)
     return user = nil if user[:errors].present?
