516954132dfb189314a583716d77e9058fe0d919
Brady Rohrig <brady.rohrig@gmail.com>
2023-05-16 16:45:49 -0700

    factories: updated user and users factories to match current BE

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     email { Faker::Internet.unique.email(name: name) }
     password { Faker::Internet.password }
     password_confirmation { password }
+    protocol_id { create(:protocol).id }
     data_sharing { Faker::Boolean.boolean}
   end
 end
